a8a87fcd8a0e8af09f30a3d085edb736d24512d7
MahmoudAhmadOsman <mahmoudosman.som@gmail.com>
2020-01-19 21:12:41 -0600

    index page is organized

--- resources/views/appointments/index.blade.php
+++ resources/views/appointments/index.blade.php
@@ -8,17 +8,16 @@
       <h1 class="text-danger text-center">Appointment Center</h1>
 
    
-            <section class="container" style="background-color: #fff">
+      <section class="container" style="background-color: #fff">
 
-                <div class="row">
-                    <div class="col-md-2 profile__panel">
-                  <div class="patient__profile">
-          <img src="http://www.writingcenter.uci.edu/wp-content/uploads/sites/28/2017/06/empty-calendar-2400px-copy.png" alt="" style="width: 120px; height: 120px;"><hr>
-             <h5>New Appointment?</h5>
-              <a href="/appointments/create" class="btn btn-outline-success btn-sm"> Create New Appointment</a>
-          
+        <div class="row">
+            <div class="col-md-2 profile__panel">
+                <div class="patient__profile">
+                    <img src="http://www.writingcenter.uci.edu/wp-content/uploads/sites/28/2017/06/empty-calendar-2400px-copy.png" alt="" style="width: 120px; height: 120px;"><hr>
+                       <h5>New Appointment?</h5>
+                        <a href="/appointments/create" class="btn btn-outline-success btn-sm"> Create New Appointment</a>        
         </div>
-                    </div>
+   </div>
  <div class="col-md-10" >
                     
   <p class="lead">Here is the list of all patients who have an appointment today</p>
@@ -83,89 +82,14 @@
              </div>
 
 
-
-
                 </div>
             </section>
-      
-  
-
-
-
-
-
-
-
-
+    
 
 <!-- ==================================
     End of the last design section
 =================================== -->
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-  <div class="container" style="display: none;">
-   <h2 class="text-danger" style="text-align: center; margin-bottom: 40px">Appointment Center</h2>
-    <div class="row">
-     
-       @if(count($appointments) > 0)
- @foreach($appointments as $appointment)
-      <div class="col-md-4" style="background-color: #fdfdfd; color: #000;">
-        <div class="patient__profile">
-          <img src="http://www.writingcenter.uci.edu/wp-content/uploads/sites/28/2017/06/empty-calendar-2400px-copy.png" alt="" style="width: 120px; height: 120px;">
- Scheduled On: {{$appointment->created_at->format('m-d-Y')}}
-          <hr>
-        </div>
-      </div>
-
-<!-- start of the center section -->
-<div class="col-md-6">
-  <h4>Appointment Type: <a href="/appointments/{{$appointment->id}}">{{$appointment->title}}</a></h4>
-            <p><b>Patient Full Name:</b> {{$appointment->name}}</p>
-            <p><b>Short Details:</b> {{$appointment->details}}</p>  
-            <p><b>Appointment Time:</b> {{$appointment->appTime}}</p>
-            <p><b>Appointment Location:</b> {{$appointment->location}}</p>
-            <p><b>Docotor Name:</b> {{$appointment->doctor}}</p>
-            <p><b>Appointment Scheduler Name:</b> {{$appointment->appMaker}}</p><hr>
-</div>
-<!-- End of the center section -->
-
-<!-- start of the right section -->
-<div class="col-md-2" style="background-color: #fdfdfd;">
-  <h5>New Appointment?</h5>
-  <a href="/appointments/create" class="btn btn-outline-success btn-sm"> Create New Appointment</a>
-  </div>
-<!-- End of the right section -->
-
-
-
-@endforeach
-{{ $appointments->links() }}
-
-@else
-        <div class="no_appointment text-center">
-          <img src="https://www.sahilgraphics.com/wp-content/themes/shahil/assets/images/image-not-found.jpg" alt="" style="width: 15%; margin: auto;">
-          <h3 class="text-danger" style="font-size: 14px;">No Appointment found!</h3>
-          <a href="/appointments/create" class="btn btn-outline-success btn-sm" style="margin-bottom: 60px;"> Create New Appointment</a>
-        </div>
-  @endif
-
-      <!-- end of foreach -->
-    </div>
-  </div>
-</section>
- 
 
 
 @endsection
