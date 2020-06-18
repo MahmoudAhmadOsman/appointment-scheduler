1a6af320ead6517a10efb68a8ee03add0fc05c13
Mahmoud Osman <mahmoudosman.som@gmail.com>
2020-06-17 19:44:44 -0500

    added Procfile for heroku

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
 
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('/css/custom.css') }}" rel="stylesheet">
 
 </head>
